0083fd3b9b8b89690263e8d30e5a962d62629d11
Ujjwal_Kumar <kingayushman7@gmail.com>
2023-09-30 18:16:28 -0800

    Save cart in localStorage

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -64,7 +64,7 @@ document.querySelector('.js-products-grid').innerHTML = productsHTML;
 function updateCartQuantity() {
   let cartQuantity = 0;
 
-  cart.forEach((item) => {
+  cart.forEach((cartItem) => {
     cartQuantity += cartItem.quantity;
   });
 
